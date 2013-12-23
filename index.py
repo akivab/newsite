d2fdf1d17e4b47680339c554a3e339db4b6550e2
Akiva Bamberger <akiva.bamberger@gmail.com>
2013-12-23 01:28:49 -0500

    Adding files for v2, which includes an editor now.

--- index.py
+++ index.py
@@ -1,12 +1,71 @@
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext import ndb
 from google.appengine.ext.webapp import template
-from django.utils import simplejson as json
 import urllib, os, re
 from google.appengine.api import mail
-from random import random
+import logging
+import json
+from models import *
 
-class MainPage(webapp.RequestHandler):
+import jinja2
+import webapp2
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+def json_dump(model):
+  return {
+    'link': model.link,
+    'title': model.title,
+    'text': model.text,
+    'page': model.page,
+    'date_posted': model.date_posted,
+    'added': str(model.added),
+    'ranking': model.ranking,
+    'images': model.images[0] if len(model.images) > 0 else '',
+    'id': model.key.id()
+  }
+
+class ItemHandler(webapp2.RequestHandler):
+  def get(self):
+    items = Item.query().fetch(1000)
+    self.response.out.write(json.dumps([json_dump(item) for item in items]))
+
+  def post(self):
+    if self.request.get('id'):
+      item = ndb.Key('Item', int(self.request.get('id'))).get()
+      if self.request.get('action') == 'DELETE':
+        item.key.delete()
+        self.response.out.write('Deleted item!')
+        return
+    else:
+      item = Item()
+      top_ranking = Item.gql('order by ranking desc limit 1').get()
+      if top_ranking:
+        item.ranking = top_ranking.ranking if top_ranking.ranking else 0
+        item.ranking += 1
+
+    if self.request.get('link'):
+      item.link = self.request.get('link')
+    if self.request.get('page'):
+      item.page = self.request.get('page')
+    if self.request.get('date_posted'):
+      item.date_posted = self.request.get('date_posted')
+    if self.request.get('images'):
+      item.images = self.request.get('images').split(',')
+    if self.request.get('ranking'):
+      item.ranking = int(self.request.get('ranking'))
+    if self.request.get('title'):
+      item.title = self.request.get('title')
+    if self.request.get('text'):
+      item.text = self.request.get('text')[:500]
+
+    item.put()
+    self.response.out.write(json.dumps(json_dump(item)))
+
+class MainHandler(webapp2.RequestHandler):
   def post(self):
     name = self.request.get("name")
     email = self.request.get("email")
@@ -17,59 +76,30 @@ class MainPage(webapp.RequestHandler):
                    subject="Message from danbamberger.com",
                    body="Sent from %s (%s, %s):\n\n%s" % (name, email, phone, message))
     self.redirect('/contact#email_received')
+
+
   def get(self):
     userAgent = self.request.headers['User-Agent']
     if "Windows" in userAgent and "MSIE" in userAgent:
       self.redirect('http://www.citi-habitats.com/agent_profile.php?id=BAM')
-    page = 'home.html'
+    page = 'home'
     template_data = {}
-    template_data['random'] = int(random() * 10**9)
+
     paths = ['buyers', 'about', 'contact', 'listings', 'thereport', 'sellers']
     for p in paths:
       if re.match('/%s' % p, self.request.path):
-        page = '%s.html' % p
-    if page == 'listings.html':
-      aptspath = os.path.join(os.path.dirname(__file__), 'data/listings/')
-      aptdir = os.listdir(aptspath)
-      goodpics = []
-      badpics = []
-      toend = ['804391.txt', '170854.txt']
-      for apt in sorted(aptdir):
-        lines = open('%s/%s' % (aptspath, apt), 'r').readlines()
-        url,title,availability,price,maintext = lines[:5]
-        img = apt[:-4]
-        color = 'green'
-        if not re.search('vailable', availability): color='red'
-        availability = '<span style="color:%s">%s</span>' % (color,availability)
-        thisapt = [{'url':url,'title':title,'img':img,'availability':availability,'price':price,'maintext':maintext}]
-        if color == 'green':
-          if apt in toend: goodpics = goodpics + thisapt
-          else: goodpics = thisapt + goodpics 
-        else:
-          if apt in toend: badpics = badpics + thisapt
-          else: badpics = thisapt + badpics
-      template_data['apts'] = goodpics + badpics
-    if page == 'thereport.html':
-      newspath = os.path.join(os.path.dirname(__file__), 'data/newsletters/')
-      newsdir = os.listdir(newspath)
-      td = []
-      for news in reversed(sorted(newsdir)):
-        lines = open('%s/%s' % (newspath, news), 'r').readlines()
-        title, url, date, img = lines[:4]
-        maintext = '\n'.join(lines[4:])[:400].replace('\n','<br>') + '...'
-        td.append({'img': img, 'maintext': maintext, 'date':date, 'url':url, 'title':title})
-      template_data['newsletters'] = td
-    template_data['PAGE'] = page
+        page = p
+
+    if page in ['listings', 'thereport']:
+      template_data['PAGE'] = 'items.html'
+      template_data['newsletters'] = Item.gql('WHERE page=:1 order by ranking desc', page).fetch(1000)
+    else:
+      template_data['PAGE'] = '%s.html' % page
     path = os.path.join(os.path.dirname(__file__), 'index.html') 
     rendered_page = template.render(path, template_data)
     self.response.out.write(rendered_page)
 
-application = webapp.WSGIApplication(
-    [('/.*', MainPage)],
+app = webapp2.WSGIApplication(
+    [('/items', ItemHandler),
+     ('/.*', MainHandler)],
      debug=False)
-
-def main():
-  run_wsgi_app(application)
-
-if __name__ == "__main__":
-  main()
